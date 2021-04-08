9f044f0558fe48dc1e39e006eed92678384b4321
joss12345 <82134338+joss12345@users.noreply.github.com>
2021-04-07 23:38:35 -0500

    Delete application_controller.rb

--- app/controllers/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-  include Clearance::Controller
-  protect_from_forgery with: :exception
-
-  def current_user
-    super || Guest.new
-  end
-end
